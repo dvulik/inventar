0206de05fbeb106f4c12afba4b10249216172097
dvulik <wassar51@gmail.com>
2023-05-12 12:10:56 +0200

    Přidejte soubory projektu.

---
+++ askjd/Class1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventar
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+    class Inventory
+    {
+        private Product[] products;
+        private int count;
+
+        public Inventory(int size)
+        {
+            products = new Product[size];
+            count = 0;
+        }
+
+        public void AddProduct(string name, double price)
+        {
+            try
+            {
+                if (price < 0)
+                {
+                    throw new Exception("cena nemuze byt zaporna");
+                }
+
+                foreach (Product p in products)
+                {
+                    if (p != null && p.Name == name)
+                    {
+                        throw new Exception("produkt jiz existuje v inventari");
+                    }
+                }
+                if (count >= products.Length)
+                {
+                    throw new Exception("inventar je plny");
+                }
+
+                products[count] = new Product { Name = name, Price = price };
+                count++;
+                Console.WriteLine("novy item v inventari: " + name + ", " + price + "kč");
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("error: " + e.Message);
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+        }
+
+        public void RemoveProduct(string name)
+        {
+            bool found = false;
+            for (int i = 0; i < count; i++)
+            {
+                if (products[i] != null && products[i].Name == name)
+                {
+                    products[i] = null;
+                    found = true;
+                    Console.WriteLine("produkt byl vymazan z inventare: " + name);
+                    break;
+                }
+            }
+            if (!found)
+                Console.WriteLine("produkt nebyl nalezen: " + name);
+        }
+    }
+}
---
+++ askjd/Program.cs
@@ -0,0 +1,17 @@
+﻿using inventar;
+
+Inventory inventory = new Inventory(6); // muzete si nastavit velikost inventare
+
+inventory.AddProduct("Jablko", 12.0);
+inventory.AddProduct("Banán", 14.0);
+inventory.AddProduct("Pomeranč", 20.0);
+inventory.AddProduct("Hruška", -1.0); // error, cena nemuze byt zaporna
+inventory.AddProduct("Ananas", 24.0);
+inventory.AddProduct("Mango", 32.0);
+inventory.AddProduct("Jablko", 12.0); // error, produkt jiz existuje v inventari
+inventory.AddProduct("Broskev", 25.0); // error, inventar je plny
+inventory.AddProduct("Meloun", 50.0);
+
+inventory.RemoveProduct("Pomeranč");
+inventory.RemoveProduct("Jablko");
+inventory.RemoveProduct("Auto"); // error, produkt neexistuje
